12c327ad7605070bf643ca95de1e4f9bd649ce88
SaiAdarsh <adarshsai619@gmail.com>
2026-03-25 11:05:30 +0530

    fix: use Express 5 wildcard syntax /{*splat} for catch-all route

--- server/src/app.js
+++ server/src/app.js
@@ -24,7 +24,7 @@ app.get('/health', (req, res) => {
 if (process.env.NODE_ENV === 'production') {
     const clientDist = path.join(__dirname, '../../client/dist');
     app.use(express.static(clientDist));
-    app.get('*', (req, res) =>
+    app.get('/{*splat}', (req, res) =>
         res.sendFile(path.join(clientDist, 'index.html'))
     );
 }
